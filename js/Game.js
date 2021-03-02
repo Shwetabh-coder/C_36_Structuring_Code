4295c4930282819b1978ec0769951b3f66bd451f
Shwetabh-coder <75842811+Shwetabh-coder@users.noreply.github.com>
2021-03-02 18:49:34 +0530

    Update Game.js

--- js/Game.js
+++ js/Game.js
@@ -1,1 +1,23 @@
-class Game{ constructor(){} getState(){ var gameStateRef=database.ref('gameState'); gameStateRef.on("value",function(data){ gameState=data.val(); }) } update(State){ database.ref('/').update({ gameState:state }); } start(){ if(gameState===0) { player=new Player(); player.getCount(); form=new Form(); form.display(); } } }
+class Game{ 
+  constructor(){} 
+  getState(){ 
+    var gameStateRef=database.ref('gameState'); 
+    gameStateRef.on("value",function(data){ 
+      gameState=data.val();
+    }) 
+  } 
+  update(State){ 
+    database.ref('/').update({ 
+      gameState:state 
+    }); 
+  } 
+  start(){ 
+    if(gameState===0) 
+    { 
+      player=new Player(); 
+      player.getCount(); 
+      form=new Form(); 
+      form.display(); 
+    } 
+  } 
+}
